26bc36c3610c3eec4f8ee7bb02aeceb4a4d7dc5b
Tomas Perez <tomasitopg6@gmail.com>
2020-08-04 16:36:31 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -58,7 +58,7 @@ class Student
       end
   end
 
-  def self.first_x_students_in_grade_10(num)
+  def self.first_X_students_in_grade_10(num)
     sql = <<-SQL
       SELECT * FROM students
       WHERE grade = 10
